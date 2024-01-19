6b2c8f8246a5877f8e37147e844b306df1a5b0c4
samanthagamero <157134047+samanthagamero@users.noreply.github.com>
2024-01-19 08:31:39 -0600

    Add files via upload

---
+++ #To do list 2.py
@@ -0,0 +1,81 @@
+#To do list
+#Samantha
+
+#Initialize
+
+myshoppinglist = ["milk", "eggs", "apples", "bread"]
+
+def add():    
+    add= input("What do vou want to add to the list?: ")
+    myshoppinglist.append(add)
+    print(myshoppinglist)
+
+def view():
+    print(myshoppinglist)
+
+def markascomplete():
+    ans = input("Select an item from the list to mark as complete: ")
+    print(ans)
+    i = myshoppinglist.index(ans)
+    print(i)
+    myshoppinglist(i) == "ans" + "x"
+    print(myshoppinglist) 
+
+def remove():
+    remove= input("What do vou want to remove from the list?: ")
+    myshoppinglist.remove(remove)
+    print(myshoppinglist)
+
+def exit():
+    exit = input("Do you want to exit the shopping list?: ")
+    if exit == "Yes":
+        print("You are exiting the shopping list.")
+    else:
+        shoppinglist()
+
+def removeall():
+    myshoppinglist.clear()
+    print(myshoppinglist)
+    
+def sorted_list():
+    sorted_list = sorted(myshoppinglist)
+    print(sorted_list)
+    
+def count():
+    print(len(myshoppinglist))
+
+
+
+def shoppinglist():
+    print("Welcome to your shopping list!")
+    print("Please select an option.")
+    option = int(input("Option: "))
+    if option == 1:
+        add()
+    if option == 2:
+        view()
+    if option == 3:
+        markascomplete()
+    if option == 4:
+        remove()
+    if option == 5:
+        exit()
+    if option == 6:
+        removeall()
+    if option == 7:
+        sorted_list()
+    if option == 8:
+        count()
+   
+
+
+
+
+    #Option 6: remove all items from list
+    
+    #Option 7: count the number of notes
+    
+    #Option 8: sort the notes alphabetically
+    
+#Main
+shoppinglist()
